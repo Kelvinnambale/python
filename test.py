ca54c6f2086125e6cd04510b3eaf5e35efeaa4e6
Kelvin Nambale <knambale5@gmail.com>
2024-09-19 12:24:50 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,3 +4,12 @@ print("Hello world")
 print("Python Indentation")
 if 5 > 2:
 	print("Five is greater than 2")
+
+print("Python variable")
+x=str(3)
+z=int(3)
+y=float(3)
+
+print(x)
+print(z)
+print(y)
