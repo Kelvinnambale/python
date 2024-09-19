395bb7465e8e4223ded2a0eb19f51c5e993262a9
Kelvin Nambale <knambale5@gmail.com>
2024-09-19 14:18:47 +0300

    Update test.py

--- test.py
+++ test.py
@@ -42,3 +42,33 @@ def myfunc():
 print("Python is " + x)
 
 
+#python numbers
+c=1
+d=2.0
+e=1j
+
+print(type(c))
+print(type(d))
+print(type(e))
+
+#type conversion
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
