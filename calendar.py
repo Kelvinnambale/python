98f671afafb81444308bc9ebb7dde0f8bf23a376
Kelvin Nambale <knambale5@gmail.com>
2024-09-28 17:52:27 +0300

    create a calendar.py

---
+++ calendar.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 28 17:18:41 2024
+
+@author: ADMIN
+"""
+
+import calendar
+
+# Input year and month
+year = int(input("Enter year: "))
+month = int(input("Enter month: "))
+
+# Display the calendar
+print(calendar.month(year, month))
