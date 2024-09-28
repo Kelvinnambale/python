c335b52d2fcea91ee702f59aee52dc725c081ce8
Kelvin Nambale <knambale5@gmail.com>
2024-09-28 16:34:41 +0300

    Update python_decision.py

--- python_decision.py
+++ python_decision.py
@@ -2,6 +2,21 @@
 """
 Created on Sat Sep 28 16:17:25 2024
 
-@author: ADMIN
+Python if statement execution
 """
+var1 = 100
+if var1:
+    print("1 - Got a true expression")
+    print(var1)
 
+var2 = 9
+if var2:
+    print("2 - - Got a true expression")
+    print(var2)
+print("Good bye")
+
+"""
+Created on Sat Sep 28 16:17:25 2024
+
+Python if else statement execution
+"""
