14a5aa1ea0e09cdf603c0167a01d7c3fd8d38eef
Kelvin Nambale <knambale5@gmail.com>
2024-09-28 20:23:23 +0300

    Update python_loop.py

--- python_loop.py
+++ python_loop.py
@@ -1,7 +1,7 @@
 """
 Python While Loop sample program
 """
-count=0
+'''count=0
 
 while (count<10):
     print("The count is ", count)
@@ -12,10 +12,19 @@ print("End of countdown")
 """
 Python infinite Loop sample program
 """
-var = 1
+#var = 1 
+#while var == 1 :  # This constructs an infinite loop 
+#   num = int(input("Enter a number  :")) 
+#   print ("You entered: ", num) 
+#print ("Good bye!")
 
-while var == 1:
-    num = int(input("Enter a Number: "))
-    print("You entered: ", num)
-print("Good bye!")
-
+"""
+Python using else statements with Loop sample program
+"""
+'''
+count=0
+while count < 5: 
+   print (count, " is  less than 5") 
+   count = count + 1 
+else: 
+   print (count, " is not less than 5") 
