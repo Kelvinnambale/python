91be29802391b95dc132bdc11f09abfaa33332e2
Kelvin Nambale <knambale5@gmail.com>
2024-10-02 17:48:26 +0300

    Update python_for_loop.py

--- python_for_loop.py
+++ python_for_loop.py
@@ -3,4 +3,16 @@ For loop example
 """
 for var in list(range(10)):
     print(var)
-     
+"""
+Python for loop example
+"""
+for letter in 'python':
+	print("Current letter",letter)
+fruits=["Banana","Apple","Orange"]
+for x in fruits:
+	print("I like ",x)
+"""
+Python iterating by sequence example
+"""
+for index in  range(len(fruits)):
+	print("")
