1ad9b68eacf039af54deff212d8f90def6cb5f7f
Kelvin Nambale <knambale5@gmail.com>
2024-10-06 16:41:38 +0300

    Create pass.py

---
+++ pass.py
@@ -0,0 +1,6 @@
+name = str(input("Enter your username: "))
+password = str(input("Enter Your password: "))
+for i in
+if password == "admin":
+    print("You've logged in successfully")
+    print("Your Username is: ",name)
