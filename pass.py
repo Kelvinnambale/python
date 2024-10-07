7967bd79d8c1d7972f6b30b5dbd979827e8a8623
Kelvin Nambale <knambale5@gmail.com>
2024-10-07 18:02:53 +0300

    Update pass.py

--- pass.py
+++ pass.py
@@ -1,6 +1,13 @@
-name = str(input("Enter your username: "))
-password = str(input("Enter Your password: "))
-for i in
-if password == "admin":
-    print("You've logged in successfully")
-    print("Your Username is: ",name)
+import sys
+list=[1,2,3,4] 
+it = iter(list) # this builds an iterator object 
+print (next(it)) #prints next available element in iterator 
+
+for x in it: 
+   print (x, end=" ") 
+
+while True: 
+   try: 
+      print (next(it)) 
+   except StopIteration: 
+      sys.exit() #you have to import sys module for this 
