830577ec79a08271f370048da28a790d3f672128
Kelvin Nambale <knambale5@gmail.com>
2024-10-08 23:01:01 +0300

    Create number.py

---
+++ number.py
@@ -0,0 +1,13 @@
+"""
+Python number ceil(function)
+"""
+import math   # This will import math module 
+print ("math.ceil(-45.17) : ", math.ceil(-45.17)) 
+print ("math.ceil(100.12) : ", math.ceil(100.12)) 
+print ("math.ceil(100.72) : ", math.ceil(100.72)) 
+print ("math.ceil(math.pi) : ", math.ceil(math.pi)) 
+
+"""
+Python number exponential method
+"""
+print("math exponential function: ",math.exp(2))
