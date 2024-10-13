bbb62db0b060adece6f84190bc61640f8ffad50b
Kelvin Nambale <knambale5@gmail.com>
2024-10-13 17:46:51 +0300

    Create trigonometric.py

---
+++ trigonometric.py
@@ -0,0 +1,18 @@
+#acos x example
+import math
+print("acos (0.2): ", math.acos(0.2))
+
+#asin()method sample
+print("asin (-0.64):", math.asin(-0.64))
+
+#atan()method sample
+print("atan(45): ", math.atan(45))
+
+#hypot () method sample
+print("hypot(4,5) ", math.hypot(4,5))
+
+#degree ()method sample
+print("45 in radians: ", math.degrees(45))
+
+#radians ()method sample
+print("45 in degrees: ", math.radians(45))
