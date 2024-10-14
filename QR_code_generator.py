41763b2bed4a4778392e53eac642e46ef8658871
Kelvin Nambale <knambale5@gmail.com>
2024-10-14 16:55:40 +0300

    Update QR_code_generator.py

--- QR_code_generator.py
+++
@@ -1,6 +0,0 @@
-import pyqrcode
-from PIL import Image
-link = input("Enter anything to generate QR code: ")
-qr_code = pyqrcode.create(link)
-qr_code.png("QRcode.png", scale=5)
-Image.open("QRcode.png") 
