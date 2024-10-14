d7b604ea9fd1d1cc59324212de0d2b9cdac6cd99
Kelvin Nambale <knambale5@gmail.com>
2024-10-14 16:55:31 +0300

    Create calendar_2.py

---
+++ calendar_2.py
@@ -0,0 +1,16 @@
+import calendar
+print(calendar.month(2024,12))
+
+print(calendar.firstweekday( ))
+
+print(calendar.isleap(2024))
+
+print(calendar.month(2024,10,w=2,l=1) )
+
+print(calendar.weekday(2024,12,14))
+
+print(calendar.setfirstweekday(4))
+
+print(calendar.monthrange(2024,4))
+
+print(calendar.prmonth(2024,6,w=2))
