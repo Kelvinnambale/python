dcd513a6037a8174e0903e024a6d50f8d71c2963
Kelvin Nambale <knambale5@gmail.com>
2024-10-14 16:55:24 +0300

    Delete calendar.py

--- calendar.py
+++
@@ -1,9 +0,0 @@
-import calendar
-
-# Input year and month
-year = int(input("Enter year: "))
-month = int(input("Enter month: "))
-
-# Display the calendar
-print(calendar.month(year, month))
- # type: ignore
