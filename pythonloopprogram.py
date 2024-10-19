927b2f7635638035c48851b9a114917fb382aa17
Kelvin Nambale <knambale5@gmail.com>
2024-10-19 08:10:10 +0300

    created python files

--- pythonloopprogram.py
+++ pythonloopprogram.py
@@ -1,12 +1,16 @@
 fname = str(input("Enter the Username: "))
 passwd = str(input("Enter the Password: "))
-count= 1
+counter= 3
 if passwd == "admin":
     print("Username: ", fname)
     print("Password", passwd)
 else:
     while passwd != "admin":
-        print("Wrong password")
-        passwd=str(input("Enter password again: "))
+        while counter !=0:
+            print("Wrong password")
+            passwd=str(input("Enter password again: "))
+            counter-=1            
+        print("Contact system administrator")
+
 print("Username: ", fname)
 print("Password", passwd)
