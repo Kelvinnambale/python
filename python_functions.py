12e1c5d8d0afdf8c0aa5a25312a4e62d88e138cb
Kelvin Nambale <knambale5@gmail.com>
2024-11-02 11:17:24 +0300

    Update python_functions.py

--- python_functions.py
+++ python_functions.py
@@ -6,3 +6,21 @@ message()
 b = int(input())
 message()
 c = int(input())
+
+#parameterized
+def hello(name): # defining a function
+    print("Hello,", name) # body of the function
+ 
+ 
+name = input("Enter your name: ")
+ 
+hello(name) # calling the function
+
+
+#positional parameter passing
+def introduction(first_name, last_name):
+    print("Hello, my name is", first_name, last_name)
+ 
+introduction("Luke", "Skywalker")
+introduction("Jesse", "Quick")
+introduction("Clark", "Kent")
