52c3c681693d3ec460fccb77947e5b69f89790be
kelvinnambale <knambale5@gmail.com>
2024-11-11 13:04:56 +0300

    classes and inheritance programs

---
+++ classes/improved_circle_client.py
@@ -0,0 +1,7 @@
+from improved_circle import circle
+
+my_circle = circle(int(input("Enter the radius of the circle: ")))
+
+print("Radius of circle: ", my_circle.set_radius())
+print("Area of circle: ", my_circle.get_area())
+print("Perimeter of circle: ", my_circle.get_perimeter())
---
+++ inheritance/rectangle_circle_client.py
@@ -0,0 +1,9 @@
+from rectangle_circle import *
+
+r1 = rectangle
+
+c1 =circle(5)
+
+def print_rectangle_info(rectangle__obj):
+    print("####################")
+    print("color of rectangle: ", rectangle__obj.get)
